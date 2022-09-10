3277a327e6b662d41df3c63cd4019b0950e0cbe1
blueocean <ginnnokizuna@yahoo.co.jp>
2022-09-10 14:34:45 +0900

    ネコのアニメーション機能拡張

--- CatchGame/src/CatLabel.java
+++ CatchGame/src/CatLabel.java
@@ -4,26 +4,46 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.Timer;
 
 //ネコのラベル
 public class CatLabel extends JLabel {
 	private static final long serialVersionUID = 1L;
-
-	// フィールド
+	//フィールド
 	Image catImage;
+	//ラベルの左上の点のx座標
+	int x;
+	//ラベルの左上の点のy座標
+	int y;
+	int xVelocity;
+	int yVelocity;
+	Timer timer = null;
 
-	// コンストラクタ
+	//コンストラクタ
 	CatLabel(){
 		// 画像の設定
 		catImage = new ImageIcon(getClass().getClassLoader().getResource("sax_cat.png")).getImage();
-		// 貼り付け先の位置とラベルサイズを設定
+		//貼り付け先の位置とラベルサイズを設定
 		this.setBounds(100,100,catImage.getWidth(null),catImage.getHeight(null));
+		//初期設定の座標と速さを決定
+		//座標
+		x = new java.util.Random().nextInt(Main.mainWindow.mainPanel.getWidth()-this.getWidth());
+		y = new java.util.Random().nextInt(Main.mainWindow.mainPanel.getWidth()-this.getWidth());
+		//速度
+		xVelocity = -5 + new java.util.Random().nextInt(11);
+		yVelocity = -5 + new java.util.Random().nextInt(11);
+		//位置設定
+		this.setLocation(x,y);
 	}
 
 	public void paint(Graphics g) {
 		super.paint(g);
-		// ネコの画像を使用
+		//ネコ画像の処理
 		Graphics2D g2D = (Graphics2D)g;
-		g2D.drawImage(catImage, 0, 0, catImage.getWidth(null), catImage.getHeight(null), null);
+		if(xVelocity >= 0) {
+			g2D.drawImage(catImage, 0, 0, catImage.getWidth(null), catImage.getHeight(null), null);
+		}else {
+			g2D.drawImage(catImage, catImage.getWidth(null), 0, -catImage.getWidth(null), catImage.getHeight(null), null);
+		}
 	}
 }
--- CatchGame/src/MainPanel.java
+++ CatchGame/src/MainPanel.java
@@ -1,49 +1,66 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class MainPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
-	// 定数フィールド
-	Color backgroundColor = Color.green;
-	// コンポネート
+	//定数フィールド
+	Color backgroundColor = Color.BLACK;
+	//コンポネート
 	CatLabel catLabel;
 	JLabel mainLabel;
-	// リスナー
+	CatLabel cat01;
+	CatLabel cat02;
+	//リスナー
 	MykeyListener mykeyListener;
 
-	// コンストラクタ
+	//コンストラクタ
 	public MainPanel() {
-		// レイアウトの設定
+		//レイアウトの設定
 		this.setLayout(null);
-		// 背景の色
+		//背景の色
 		this.setBackground(backgroundColor);
 	}
 
-	// コンポネートの処理
+	//コンポネートの処理
 	public void prepareComponents() {
-		// 各ラベル生成
+		//各ラベル生成
 		catLabel = new CatLabel();
 		mainLabel = new JLabel();
-		// メインラベルに文字を記載？
+		cat01 = new CatLabel();
+		vitalizeCat(cat01);
+		cat02 = new CatLabel();
+		vitalizeCat(cat02);
+		//メインラベルに文字を記載？
 		mainLabel.setText("");
-		// メインラベルの位置とサイズを指定
+		//メインラベルの位置とサイズを指定
 		mainLabel.setBounds(100,200,100,30);
-		// 各ラベルを対象パネルに貼る
+		//各ラベルを対象パネルに貼る
 		this.add(catLabel);
 		this.add(mainLabel);
 		mykeyListener = new MykeyListener(this);
 	}
 
+	//ネコにリスナーをつけてからパネルに貼る処理
+	public void vitalizeCat(CatLabel c) {
+		CatActionListener catListener = new CatActionListener(c);
+		c.timer = new Timer(10,catListener);
+		this.add(c);
+		c.timer.start();
+	}
+
 	//ネコの制御(内部クラス)
 	private class MykeyListener implements KeyListener{
-		// 貼りつけ先を保持
+		//貼りつけ先を保持
 		MainPanel panel;
-		// コンストラクタ
+		//コンストラクタ
 		MykeyListener(MainPanel p){
 		super();
 		panel = p;
@@ -52,57 +69,57 @@ public class MainPanel extends JPanel {
 
 		@Override
 		public void keyTyped(KeyEvent e) {
-			// do nothing
+			//do nothing
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			// do nothing
+			//do nothing
 		}
 
 		@Override
 		public void keyPressed(KeyEvent e) {
-			// 壁にぶつかったら止まる処理
+			//壁にぶつかったら止まる処理
 			switch(e.getKeyCode()) {
-			// 上を押した場合かつ
+			//上を押した場合かつ
 			case KeyEvent.VK_UP:
-				// Y座標が0より大きい場合 → 画面の一番上より上の場合
+				//Y座標が0より大きい場合 → 画面の一番上より上の場合
 				if(catLabel.getY() > 0) {
-					// Y軸にマイナス5をした場所に設置
+					//Y軸にマイナス5をした場所に設置
 					catLabel.setLocation(catLabel.getX(),catLabel.getY()-5);
 				}
 				break;
 
-			// 下を押した場合かつ
+			//下を押した場合かつ
 			case KeyEvent.VK_DOWN:
-				// Y座標がパネルの位置より下な場合 → 画面の一番下より下の場合
-				// catLabel.catImage.getHeight(null) 左記のnullは何を指している？？
+				//Y座標がパネルの位置より下な場合 → 画面の一番下より下の場合
+				//catLabel.catImage.getHeight(null) 左記のnullは何を指している？？
 				if(catLabel.getY() + catLabel.catImage.getHeight(null) < panel.getHeight()) {
-					// Y軸にプラス5をした場所に設置
+					//Y軸にプラス5をした場所に設置
 					catLabel.setLocation(catLabel.getX(),catLabel.getY()+5);
 				}
 				break;
 
-			// 左を押した場合かつ
+			//左を押した場合かつ
 			case KeyEvent.VK_LEFT:
-				// X座標が0より大きい場合 → 画面の一番左より左の場合
+				//X座標が0より大きい場合 → 画面の一番左より左の場合
 				if(catLabel.getX() > 0) {
-					// X軸にマイナス5をした場所に設置
+					//X軸にマイナス5をした場所に設置
 					catLabel.setLocation(catLabel.getX()-5,catLabel.getY());
 				}
 				break;
 
-			// 右を押した場合かつ
+			//右を押した場合かつ
 			case KeyEvent.VK_RIGHT:
-				// X座標がパネルの位置より右な場合 → 画面の一番右より右の場合
-				// catLabel.catImage.getWidth(null) 左記のnullは何を指している？？
+				//X座標がパネルの位置より右な場合 → 画面の一番右より右の場合
+				//catLabel.catImage.getWidth(null) 左記のnullは何を指している？？
 				if(catLabel.getX() + catLabel.catImage.getWidth(null) < panel.getWidth()) {
-					// X軸にプラス5をした場所に設置
+					//X軸にプラス5をした場所に設置
 					catLabel.setLocation(catLabel.getX()+5,catLabel.getY());
 				}
 				break;
 
-			// Enterを押した場合
+			//Enterを押した場合
 			case KeyEvent.VK_ENTER:
 				if(backgroundColor == Color.green) {
 					backgroundColor = Color.blue;
@@ -115,4 +132,30 @@ public class MainPanel extends JPanel {
 			}
 		}
 	}
+
+	//パネル内でネコを走らせるクラス(内部クラス)
+	private class CatActionListener implements ActionListener{
+		private CatLabel cat;
+
+		public CatActionListener(CatLabel c) {
+			cat = c;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(cat.x > Main.mainWindow.mainPanel.getWidth() - cat.getWidth() || cat.x < 0) {
+				cat.xVelocity = cat.xVelocity * (-1);
+			}
+			cat.x = cat.x + cat.xVelocity;
+			if(cat.y > Main.mainWindow.mainPanel.getWidth() - cat.getWidth() || cat.y < 0) {
+				cat.yVelocity = cat.yVelocity * (-1);
+			}
+			cat.y = cat.y + cat.yVelocity;
+			cat.setLocation(cat.x,cat.y);
+			cat.repaint();
+
+		}
+
+	}
+
 }
