f3932a05c5d9590c159420d6ed6592a36c2a98ab
blueocean <ginnnokizuna@yahoo.co.jp>
2022-09-14 19:54:40 +0900

    マウス操作機能の追加

--- CatchGame/src/MainPanel.java
+++ CatchGame/src/MainPanel.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,7 +21,7 @@ public class MainPanel extends JPanel {
 	CatLabel cat01;
 	CatLabel cat02;
 	//リスナー
-	MykeyListener mykeyListener;
+	MykeyListener mykeyListener; //不動ネコ
 
 	//コンストラクタ
 	public MainPanel() {
@@ -43,20 +45,21 @@ public class MainPanel extends JPanel {
 		//メインラベルの位置とサイズを指定
 		mainLabel.setBounds(100,200,100,30);
 		//各ラベルを対象パネルに貼る
-		this.add(catLabel);
-		this.add(mainLabel);
-		mykeyListener = new MykeyListener(this);
+		this.add(catLabel); //不動ネコ
+		this.add(mainLabel); //不動ネコ
+		mykeyListener = new MykeyListener(this); //不動ネコ
 	}
 
 	//ネコにリスナーをつけてからパネルに貼る処理
 	public void vitalizeCat(CatLabel c) {
+		new DDListener(c);
 		CatActionListener catListener = new CatActionListener(c);
 		c.timer = new Timer(10,catListener);
 		this.add(c);
 		c.timer.start();
 	}
 
-	//ネコの制御(内部クラス)
+	//ネコの制御(内部クラス) //不動ネコ
 	private class MykeyListener implements KeyListener{
 		//貼りつけ先を保持
 		MainPanel panel;
@@ -147,7 +150,7 @@ public class MainPanel extends JPanel {
 				cat.xVelocity = cat.xVelocity * (-1);
 			}
 			cat.x = cat.x + cat.xVelocity;
-			if(cat.y > Main.mainWindow.mainPanel.getWidth() - cat.getWidth() || cat.y < 0) {
+			if(cat.y > Main.mainWindow.mainPanel.getHeight() - cat.getHeight() || cat.y < 0) {
 				cat.yVelocity = cat.yVelocity * (-1);
 			}
 			cat.y = cat.y + cat.yVelocity;
@@ -157,5 +160,53 @@ public class MainPanel extends JPanel {
 		}
 
 	}
+
+	//マウス操作_ドラッグアンドドロップ（内部クラス）
+	private class DDListener extends MouseAdapter{
+		private int dx;
+		private int dy;
+		private CatLabel cat;
+
+		//コンストラクタ
+		DDListener(CatLabel c){
+			cat = c;
+			cat.addMouseListener(this);
+			cat.addMouseMotionListener(this);
+		}
+
+		//マウスの長押し処理
+		public void mousePressed(MouseEvent e) {
+			cat.timer.stop();
+			//押さえたところからラベルの左上の差を取っておく
+			dx = e.getXOnScreen() - cat.getX();
+			dy = e.getYOnScreen() - cat.getY();
+		}
+
+		//マウスのドラッグ処理
+		public void mouseDragged(MouseEvent e) {
+			//マウスの座標からラベルの左上の座標を取得
+			int x = e.getXOnScreen() - dx;
+			int y = e.getXOnScreen() - dy;
+			if(x < Main.mainWindow.mainPanel.getWidth() - cat.getWidth() - 5 && x > 5) { cat.x = x;}
+			if(y < Main.mainWindow.mainPanel.getHeight() - cat.getHeight() - 5 && y > 5) { cat.y = y;}
+			cat.setLocation(cat.x,cat.y);
+		}
+
+		//マウスのクリック処理
+		public void mouseClicked(MouseEvent e) {
+			if(e.getClickCount() >= 2) {
+				//ダブルクリック
+				Main.mainWindow.mainPanel.remove(e.getComponent());
+				repaint();
+			}
+		}
+
+		//マウスをはなした時の処理
+		public void mouseReleased(MouseEvent e) {
+			cat.timer.start();
+		}
+
+	}
+
 
 }
