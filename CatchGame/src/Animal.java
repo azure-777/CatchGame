e0efc94df9665b34e35436b8a0a5108fbd187653
blueocean <ginnnokizuna@yahoo.co.jp>
2022-09-27 22:49:13 +0900

    コメント追加

--- CatchGame/src/Animal.java
+++ CatchGame/src/Animal.java
@@ -43,7 +43,7 @@ public abstract class Animal extends JLabel {
 		this.setBounds(x,y,image.getWidth(null),image.getHeight(null));
 	}
 
-	//設定
+	//設定(抽象メソッド)
 	protected abstract void prepareImageAndScoreAndVoice();
 
 	//画像の動作処理
